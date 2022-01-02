89fec86ce326d9de6e846716f514c3fd32a571a7
seriousKang <scdean110@gmail.com>
2022-01-02 23:21:45 +0900

    POST HTTP Method - API implementation for user registration

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,10 +2,12 @@ package com.example.restfulwebservice.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private Integer id;
--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,11 +1,11 @@
 package com.example.restfulwebservice.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class UserController {
     private UserDaoService userDaoService;
@@ -22,5 +22,11 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        log.info("user = {}", user);
+        userDaoService.save(user);
     }
 }
