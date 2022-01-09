0fa73db813eab2cc49714ec1a7d4c962a2e8e621
seriousKang <scdean110@gmail.com>
2022-01-09 13:36:33 +0900

    swagger implementation

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,8 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +15,17 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 //@JsonFilter("UserInfo") // 필터 ID
+@ApiModel(description = "Domain object for user details")
 public class User {
     private Integer id;
     @Size(min = 2, message = "Enter at least 2 characters.")
+    @ApiModelProperty(notes = "Please enter your name")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Please enter your join date")
     private Date joinDate;
+    @ApiModelProperty(notes = "Please enter your password")
     private String password;
+    @ApiModelProperty(notes = "Please enter your social security number")
     private String ssn;
 }
